5cad526f9f8a4d678829cccbdff1ac555774dd7f
sstuden <sstuden@cs.washington.edu>
2024-02-02 09:33:33 -0800

    Initial Commit

---
+++ ProjectProgress/ProjectProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectProgressApp.swift
+//  ProjectProgress
+//
+//  Created by Simon Studen on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
